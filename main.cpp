0a46aae54f7caa7022592f85f7ae821fd2ef4886
Alejandro Sanchez <alesanchezyo@gmail.com>
2025-11-09 18:25:00 -0300

    Version 1 SA.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <random>
+#include <cmath>
 
 struct Pieza{
     int indice;
-    int alto;
     int ancho;
+    int alto;
     int posX;
     int posY;
     bool rot;
@@ -25,7 +27,7 @@ struct Solucion{
 };
 
 int n,W; //cant piezas, Ancho tira
-int H; //Altura a minimizar
+mt19937 rng(02122003);
 
 vector<Pieza> leerInstancia(string nombreArchivo){
     
@@ -72,11 +74,25 @@ void infoInicialInstancia(vector<Pieza> &piezas){
     }
 }
 
-bool dentroTira(const Pieza &p){
-    return (p.posX >= 0 && p.posY >= 0 && p.posX + p.ancho <= W && p.posY + p.alto <= H);
+void infoAlturasColumnas(Solucion &sol){
+    for(int i = 0; i < W; i++){
+        cout<<"Altura en la columna "<<i+1<<" es "<<sol.alturaColumnas[i]<<endl;
+    }
 }
 
-int calcularAlturaMax(const vector<Pieza> &piezas){
+void infoCoordPiezas(Solucion &sol){
+    for(int i = 0; i < n; i++){
+        cout<<"Coordenadas pieza "<<i<<":"<<sol.piezas[i].posX<<","<<sol.piezas[i].posY<<endl;
+    }
+}
+
+void infoOrden(Solucion &sol){
+    for(int i = 0; i < n; i++){
+        cout<<sol.orden[i]<<endl;
+    }
+}
+
+int calcularAlturaMax(vector<Pieza> &piezas){
     int alt = 0;
 
     for(auto& p : piezas){
@@ -89,38 +105,6 @@ int calcularAlturaMax(const vector<Pieza> &piezas){
     return alt;
 }
 
-
-int evaluar(const vector<int> &orden, const vector<Pieza> &piezas){
-
-    vector<int> alturaColumnas(W,0);
-    int H_sol = 0;
-    for(int j = 0; j < n; j++){
-        int anchoPieza = piezas[orden[j]].ancho;
-        int altoPieza = piezas[orden[j]].alto;
-        int limite = W - anchoPieza;
-        int x_best = -1;
-        int y_best = 100000;
-            
-        for(int x = 0; x <= limite; x++){
-            int y_pos = 0;
-            for(int k = x; k < x + anchoPieza; k++){
-                y_pos = max(y_pos, alturaColumnas[k]);
-            }
-
-            if(y_pos < y_best){
-                y_best = y_pos;
-                x_best = x;
-            }
-        }
-
-        for(int x2 = x_best; x2 < x_best + anchoPieza; x2++){
-            alturaColumnas[x2] = y_best + altoPieza;
-            H_sol = max(H_sol, y_best + altoPieza);
-        }
-    }
-
-    return H_sol;
-}
 
 void evaluar(Solucion &sol){
 
@@ -160,6 +144,36 @@ void evaluar(Solucion &sol){
     
 }
 
+
+void hacerMovimiento(Solucion &sol){
+
+    uniform_int_distribution<int> distMov(0, 1); //swap = 0, rotación = 1
+    uniform_int_distribution<int> distInd(0, n-1);
+
+    int tipo = distMov(rng);
+
+    if(tipo == 0){
+        
+        int i = distInd(rng);
+        int j = distInd(rng);
+        while(i == j){
+            j = distInd(rng);
+        }
+
+        swap(sol.orden[i], sol.orden[j]);
+        cout<<"Se hizo un swap entre "<<sol.orden[j]<<" y "<<sol.orden[i]<<endl;
+    }else{
+
+        int i = distInd(rng);
+        sol.piezas[sol.orden[i]].rot = !sol.piezas[sol.orden[i]].rot;
+        cout<<"Se va a hacer una rotación en la pieza "<<sol.orden[i] + 1<<endl;
+        cout<<"Ancho antes de la rot:"<< sol.piezas[sol.orden[i]].ancho<<endl;
+        swap(sol.piezas[sol.orden[i]].ancho, sol.piezas[sol.orden[i]].alto);
+        sol.piezas[sol.orden[i]].cantRot += 1;
+        cout<<"Ancho después de la rot:"<< sol.piezas[sol.orden[i]].ancho<<endl;
+    }
+}
+
 int main(){
 
     string nombreArchivo;
@@ -168,13 +182,12 @@ int main(){
 
     nombreArchivo = "BENG01.TXT";
     vector<Pieza> piezas = leerInstancia(nombreArchivo);
-    infoInicialInstancia(piezas);
+    //infoInicialInstancia(piezas);
 
     int H_max = calcularAlturaMax(piezas);
     cout<<"Altura maxima es: "<<H_max<<endl;
 
-    H = 0;
-
+    
     vector<int> orden(n);
     
     
@@ -182,22 +195,54 @@ int main(){
         orden[i] = i;
     }
 
-    Solucion sol_inicial;
-    sol_inicial.orden = orden;
-    sol_inicial.piezas = piezas;
+    shuffle(orden.begin(), orden.end(), rng);
 
-    int H1 = evaluar(orden, piezas);
-    evaluar(sol_inicial);
-    int H2 = sol_inicial.H_sol;
-    /*for(int i = 0; i < W; i++){
-        cout<<"Altura en la columna "<<i+1<<" es "<<alturaColumnas[i]<<endl;
-    }*/
+    
+    Solucion sol_actual;
+    sol_actual.orden = orden;
+    sol_actual.piezas = piezas;
 
-    cout<<"H1 es: "<<H1<<endl;
-    cout<<"H2 es: "<<H2<<endl;
+    evaluar(sol_actual);
+    int H = sol_actual.H_sol;
+    
+    cout<<"H es: "<<H<<endl;
 
-    for(int i = 0; i < n; i++){
-        cout<<"Coordenadas pieza "<<i<<":"<<sol_inicial.piezas[i].posX<<","<<sol_inicial.piezas[i].posY<<endl;
+    Solucion mejorSol = sol_actual;
+    Solucion sol_vecino;
+    float temp = 10.;
+    float alpha = 0.9;
+    float probabilidad;
+    uniform_real_distribution<double> dist01(0.0, 1.0);
+
+    for(int i = 0; i < 10; i++){
+        sol_vecino = sol_actual;
+        hacerMovimiento(sol_vecino);
+        evaluar(sol_vecino);
+        cout<<"Este es el H de la sol actual en la iter "<<i+1<<":"<<sol_actual.H_sol<<endl;
+        cout<<"Este es el H de la sol vecina en la iter "<<i+1<<":"<<sol_vecino.H_sol<<endl;
+        if(sol_vecino.H_sol < sol_actual.H_sol){
+            sol_actual = sol_vecino;
+            cout<<"Se acepto la sol vecina"<<endl;
+            cout<<"El nuevo valor de H sol actual:"<<sol_actual.H_sol<<endl;
+        }else{
+            cout<<"Se esta viendo si se acepta"<<endl;
+
+            probabilidad = exp(((sol_actual.H_sol - sol_vecino.H_sol)/temp));
+            cout<<"Prob:"<<probabilidad<<endl;
+            //si el numero al azar entre 0. y 1.0 es menor a probabilidad se acepta    
+            if( dist01(rng) < probabilidad){
+                sol_actual = sol_vecino;
+                cout<<"Se acepto la sol vecina en la 2da oportunidad"<<endl;
+                cout<<"El nuevo valor de H sol actual:"<<sol_actual.H_sol<<endl;
+            }
+        }
+
+        if(sol_vecino.H_sol < mejorSol.H_sol){
+            mejorSol = sol_vecino;
+        }
+
+        temp = alpha * temp;
     }
 
+    cout<<"Este es el H minimo encontrado: "<<mejorSol.H_sol<<endl;
 }
